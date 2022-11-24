3e341cc788fbda386a45f178ad3172dfd7862ef5
LemonRiz <95.m.rizvi@gmail.com>
2022-11-24 10:37:27 +0000

    favicon added

--- main.js
+++ main.js
@@ -156,6 +156,7 @@ bDec.addEventListener("click", () => {
 });
 
 // Can only change first num to negative
+// makes calculator act strange if the operator is ("-")
 bPosneg.addEventListener("click", () => {
   num1 = parseFloat(display.innerHTML);
   if (display.innerHTML.includes("-")) {
